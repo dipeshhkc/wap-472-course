38dab119276de52b6d516d55acbe79285cc09422
dipesh <kcdipesh429@gmail.com>
2023-08-05 17:48:36 -0500

    feat: add maze page

---
+++ js/maze.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+  let gameInProgress = false;
+
+  const startGame = () => {
+    gameInProgress = true;
+    if ($('.boundary').hasClass('youlose')) {
+      $('.boundary').removeClass('youlose');
+    }
+    $('#status').text('Game Started! Navigate through the maze');
+  };
+
+  const endGame = (msg) => {
+    if (gameInProgress) {
+      gameInProgress = false;
+      $('#status').text(msg);
+      alert(msg);
+    }
+  };
+
+  const loseGame = () => {
+    if (gameInProgress) {
+      gameInProgress = false;
+      $('.boundary').addClass('youlose');
+      $('#status').text('Oops! You hit the boundary. Game Lost.');
+      alert('Oops! You hit the boundary. Game Lost.');
+    }
+  };
+
+  $('#start').click(startGame);
+  $('#maze .boundary').mouseenter(loseGame);
+  $('#maze').mouseleave(() => endGame('You left the battlefield! Try again.'));
+  $('#end').mouseenter(() => endGame('Hurray!! You won the game.'));
+});
